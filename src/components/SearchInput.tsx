08efd5094492ccb219b79a834855c1a202a5c8c9
Petar Yankov <yankov.petar4@gmail.com>
2024-09-16 18:51:20 +0300

    refactoring of programming navigation

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,23 +1,10 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-
+import { Form } from "react-router-dom";
 
 export default function SearchInput() {
-    const [term, setTerm] = useState("");
-    const navigate = useNavigate();
-
-    const handleSubmith = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-
-        navigate(`/search?term=${term}`);
-    };
 
     return (
-        <form onSubmit={handleSubmith}>
-            <input
-                value={term}
-                onChange={e => setTerm(e.target.value)}
-            />
-        </form>
+        <Form action="/search">
+            <input name="term" />
+        </Form>
     );
 }
