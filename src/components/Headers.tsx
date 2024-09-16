5aea23fa3ec27972fc0cb74346c45b79d3458f5c
Petar Yankov <yankov.petar4@gmail.com>
2024-09-16 19:08:04 +0300

    styling added

--- src/components/Headers.tsx
+++ src/components/Headers.tsx
@@ -3,9 +3,13 @@ import SearchInput from "./SearchInput";
 
 export default function Header() {
     return (
-        <div>
-            <Link to={"/"}>NPM Regitry</Link>
-            <SearchInput />
+        <div className="flex items-center justify-between px-4 border-b h-14">
+            <div className="flex items-center space-x-2 text-sm">
+                <Link className="text-lg font-bold" to={"/"}>NPM Regitry</Link>
+            </div>
+            <div className="w-full max-w-xl ml-4">
+                <SearchInput />
+            </div>
         </div>
     );
 }
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,10 +1,20 @@
+import { VscSearch } from "react-icons/vsc";
 import { Form } from "react-router-dom";
 
 export default function SearchInput() {
 
     return (
         <Form action="/search">
-            <input name="term" />
+            <div className="relative">
+                <div className="absolute inset-y-0 flex items-center pl-3">
+                    <VscSearch className="h-5 w-5 text-gray-500" />
+                </div>
+                <input
+                    name="term"
+                    className="pl-10 py-2 w-full border-0 shadow-none"
+                    placeholder="Search packeges"
+                />
+            </div>
         </Form>
     );
 }
